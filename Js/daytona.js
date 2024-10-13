df05c4260f72749b84788d9dcb1044f93af3f042
gtanishka100 <gtanishka100@gmail.com>
2024-10-13 09:23:58 +0530

    Added controls

--- Js/daytona.js
+++ Js/daytona.js
@@ -3,3 +3,9 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const startGameButton = document.getElementById("playButton");
+
+startGameButton.addEventListener("click", function () {
+  window.location.href = "html/game.html";
+});
